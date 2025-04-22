17f1e508193bfb00182e1c83c6bcd8c544ce1c5a
kiannosratian <kiannosratian76@gmail.com>
2025-04-23 01:30:21 +0330

    start coding  with vim editor

--- main.c
+++ main.c
@@ -4,39 +4,8 @@
  #include <termios.h>
 
 
-char getch (){
-	struct termios oldt , newt;
-	char ch;
-
-	tcgetattr(STDIN_FILENO,&oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO );
-	tcsetattr (STDIN_FILENO , TCSANOW , &newt);
-
-
-	ch =getchar();
-
-	tcsetattr(STDIN_FILENO , TCSANOW , &oldt);
-
-	return ch;
-	
-}
-
-
-
-
-
-
 int  main(){
-	
-	char ch; 
-	
-	printf("press a key: ");
-
-	ch = getchar();
-
-	printf("pressed this :%c\n ", ch );
-	
+	engin();	
 
 	return 0;
 }
--- src/engine.c
+++ src/engine.c
@@ -1,9 +1,54 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+struct termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO , TCSAFLUSH , &orig_termios);
+
+}
+
+
+
+void enableRawMode(){
+	tcgetattr(STDIN_FILENO , &orig_termios);
+	atexit(disableRawMode);
+
+
+
+
+	struct termios raw = orig_termios;
+
+	
+	raw.c_iflag &= ~(ECHO | ICANON ); 
+
+	tcsetattr(STDIN_FILENO ,TCSAFLUSH, &raw);
+
+}
 
 
 
 int engin() {
-	printf("hi i am engin\n");
+
+	enableRawMode();
+
+	char c ; 
+	while(read(STDIN_FILENO , &c , 1 ) == 1  && c != 'q' ){
+		if(iscntrl(c)){
+			printf("%d\n",c);
+		}else{
+			printf("%d('%c')\n",c,c);
+		}
+	}
+
+	return 0;
+
+
+
 }
 
 
